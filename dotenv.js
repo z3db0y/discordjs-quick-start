ad010075fbfbd7cea0a2b775ef34fd55fa0e8696
z3db0y <us3r0002@gmail.com>
2022-06-11 19:52:51 +0300

    Fix dotenv finally

--- dotenv.js
+++ dotenv.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 if(fs.existsSync('.env') && fs.statSync('.env').isFile()) {
     fs.readFileSync('.env').toString().split('\n').forEach(line => {
-        const key = line.split('=')[0];
-        const value = line.split('=').slice(1).join('=');
+        const [key, value] = line.split('=').map(x => x.trim());
         if(key && value) {
             if(value.startsWith('"') && value.endsWith('"')) value = value.slice(1, -1);
             if(value.startsWith("'") && value.endsWith("'")) value = value.slice(1, -1);
