cc314904ba4e62d1d1ca45885b8f8a6164c4b60f
z3db0y <us3r0002@gmail.com>
2022-06-11 19:48:56 +0300

    Don't use dotenv for now

--- dotenv.js
+++ dotenv.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 if(fs.existsSync('.env') && fs.statSync('.env').isFile()) {
     fs.readFileSync('.env').toString().split('\n').forEach(line => {
-        const [key, value] = line.split('=');
-        if(value.startswith('"') && value.endsWith('"')) value = value.slice(1, -1);
-        if(value.startswith("'") && value.endsWith("'")) value = value.slice(1, -1);
-        if(value.startswith('`') && value.endsWith('`')) value = value.slice(1, -1);
-        if(key && value) process.env[key] = value;
+        const key = line.split('=')[0];
+        const value = line.split('=').slice(1).join('=');
+        if(key && value) {
+            if(value.startsWith('"') && value.endsWith('"')) value = value.slice(1, -1);
+            if(value.startsWith("'") && value.endsWith("'")) value = value.slice(1, -1);
+            if(value.startsWith('`') && value.endsWith('`')) value = value.slice(1, -1);
+            process.env[key] = value;
+        }
     });
 }
