48b087039af5cf5797ebbc4e9ac84d4320a0a819
z3db0y <us3r0002@gmail.com>
2022-06-11 18:34:50 +0300

    Add dotenv support in case you need it.

---
+++ dotenv.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if(fs.existsSync('.env') && fs.statSync('.env').isFile()) {
+    fs.readFileSync('.env').split('\n').forEach(line => {
+        const [key, value] = line.split('=');
+        if(value.startswith('"') && value.endsWith('"')) value = value.slice(1, -1);
+        if(value.startswith("'") && value.endsWith("'")) value = value.slice(1, -1);
+        if(value.startswith('`') && value.endsWith('`')) value = value.slice(1, -1);
+        if(key && value) process.env[key] = value;
+    });
+}
